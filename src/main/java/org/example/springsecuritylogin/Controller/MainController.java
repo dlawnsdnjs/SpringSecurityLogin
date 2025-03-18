e86f377cc19a1bf9aada2a5a749409a7765ccaa5
dlawnsdnjs <59200605+dlawnsdnjs@users.noreply.github.com>
2025-03-18 15:20:52 +0900

    main controller add

--- src/main/java/org/example/springsecuritylogin/Controller/MainController.java
+++ src/main/java/org/example/springsecuritylogin/Controller/MainController.java
@@ -1,2 +1,13 @@
-package org.example.springsecuritylogin.Controller;public class MainController {
+package org.example.springsecuritylogin.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String main(){
+        return "main";
+    }
 }
